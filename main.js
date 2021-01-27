1e77d9e639332f9d53e61df6bb4a55e7a177cb9c
procheta1999 <bhattacharyya.procheta1999@gmail.com>
2021-01-27 23:37:55 +0530

    final

--- main.js
+++ main.js
@@ -1,24 +1,36 @@
 const gameScene= new Phaser.Scene('Game');
+var s=0;
+var fx;
 function myFunction() {
   location.reload();
 }
 gameScene.preload=function(){
-    this.load.image('background', 'assets/background_new.png');
+    this.load.image('background', 'assets/sky.jpg');
     this.load.image('ball', 'assets/ball.png');
     this.load.image('balloon', 'assets/ball.png');
-    this.load.image('arrow','assets/arrow_mod.png');
+    this.load.image('blue', 'assets/blue.png');
+    this.load.image('green', 'assets/green.png');
+    this.load.image('yellow', 'assets/yellow.png');
+    this.load.image('arrow','assets/arrow_new.png');
+    this.load.audio('sfx', 'assets/shotgun.wav');
 };   
 gameScene.create=function(){
     this.background=this.add.image(120,120,'background');
+    this.sound.add('sfx');
     this.background.setScale(10,10);
+    this.text = this.add.text(300,700, "Your Score:", {
+            font: "bold 30px Arial",
+            align: "center",
+            color: "black"
+        });
    this.ball=this.add.sprite(130,140,'ball');
-    this.balloon=this.add.sprite(210,140,'balloon');
-    this.balloon1=this.add.sprite(290,140,'balloon');
-     this.balloon2=this.add.sprite(200,280,'balloon');
-    this.balloon3=this.add.sprite(210,400,'balloon');
-    this.balloon4=this.add.sprite(130,350,'balloon');
+    this.balloon=this.add.sprite(210,140,'yellow');
+    this.balloon1=this.add.sprite(290,140,'blue');
+     this.balloon2=this.add.sprite(200,280,'green');
+    this.balloon3=this.add.sprite(210,400,'blue');
+    this.balloon4=this.add.sprite(130,350,'green');
     this.balloon5=this.add.sprite(290,370,'balloon');
-    this.balloon6=this.add.sprite(130,400,'balloon');
+    this.balloon6=this.add.sprite(130,400,'yellow');
     
     // this.ball=this.add.sprite(150,180,'ball',{align:"center"});
      this.ball.setScale(0.5,0.5);
@@ -38,45 +50,88 @@ gameScene.create=function(){
     this.physics.add.existing(this.balloon5);
     this.physics.add.existing(this.balloon6);
     this.arrow=this.add.sprite(130,800,'arrow');
+    this.arrow.angle=45;
     this.physics.add.existing(this.arrow);
     
     this.move=true;
     this.physics.add.collider(this.ball, this.arrow, (ball, arrow) => {
+        this.sound.play('sfx');
     ball.destroy();
+        s=s+1;
+        this.text.setText("Your score: "+s);
+        console.log('s',s);
+        this.arrow.body.velocity.set(0,-1000);
 }, null, this);
     this.physics.add.collider(this.balloon, this.arrow, (ball, arrow) => {
+        this.sound.play('sfx');
     ball.destroy();
+        s=s+1;
+        this.text.setText("Your score: "+s);
+        console.log('s',s);
+        this.arrow.body.velocity.set(0,-1000);
 }, null, this);
     this.physics.add.collider(this.balloon1, this.arrow, (ball, arrow) => {
+        this.sound.play('sfx');
     ball.destroy();
+        s=s+1;
+        this.text.setText("Your score: "+s);
+        console.log('s',s);
+        this.arrow.body.velocity.set(0,-1000);
 }, null, this);
     this.physics.add.collider(this.balloon2, this.arrow, (ball, arrow) => {
+        this.sound.play('sfx');
     ball.destroy();
+        s=s+1;
+        this.text.setText("Your score: "+s);
+        console.log('s',s);
+        this.arrow.body.velocity.set(0,-1000);
 }, null, this);
     this.physics.add.collider(this.balloon3, this.arrow, (ball, arrow) => {
+        this.sound.play('sfx');
     ball.destroy();
+        s=s+1;
+        this.text.setText("Your score: "+s);
+        console.log('s',s);
+        this.arrow.body.velocity.set(0,-1000);
 }, null, this);
     this.physics.add.collider(this.balloon4, this.arrow, (ball, arrow) => {
+        this.sound.play('sfx');
     ball.destroy();
+        s=s+1;
+        this.text.setText("Your score: "+s);
+        console.log('s',s);
+        this.arrow.body.velocity.set(0,-1000);
 }, null,this);
     this.physics.add.collider(this.balloon5, this.arrow, (ball, arrow) => {
+        this.sound.play('sfx');
     ball.destroy();
+        s=s+1;
+        this.text.setText("Your score: "+s);
+        console.log('s',s);
+        this.arrow.body.velocity.set(0,-1000);
 }, null, this);
     this.physics.add.collider(this.balloon6, this.arrow, (ball, arrow) => {
+        this.sound.play('sfx');
     ball.destroy();
+        s=s+1;
+        this.text.setText("Your score: "+s);
+        console.log('s',s);
+        this.arrow.body.velocity.set(0,-1000);
 }, null, this);
-   
+    
     //  this.physics.add.existing(this.ball);
    
 };
         gameScene.update=function(){
             if(this.input.activePointer.isDown)
+               
                 {
                     if(this.move)
                         {
-                          this.arrow.body.velocity.set(170, -500);
+                          this.arrow.body.velocity.set(0,-1000);
+                            
                             console.log(this.arrow.x);
-                            if(this.arrow.x>200)
+                            if(this.arrow.x<200)
                                 {
                                     this.arrow.x=130;
                                     this.arrow.y=800;
@@ -86,44 +141,55 @@ gameScene.create=function(){
                     
                 }
              
-            this.ball.x+=2;
-            if(this.ball.x>900)
+            this.ball.x+=3;
+            if(this.ball.x>800)
                 {
                     this.ball.x=-150;
                 }
-           this.balloon.x+=3;
-            if(this.balloon.x>900)
+            this.balloon1.x+=2;
+            if(this.balloon1.x>800)
+                {
+                    this.balloon1.x=-150;
+                }
+           this.balloon.x+=5;
+            if(this.balloon.x>800)
                 {
                     this.balloon.x=-150;
                 }
-            this.balloon2.x+=1;
-            if(this.balloon2.x>900)
+            this.balloon2.x+=4;
+            if(this.balloon2.x>700)
                 {
                     this.balloon2.x=-150;
                 }
-            this.balloon3.x+=0.5;
-            if(this.balloon3.x>900)
+            this.balloon3.x+=3;
+            if(this.balloon3.x>500)
                 {
                     this.balloon3.x=-150;
                 }
             
-             this.balloon4.x+=0.3;
-            if(this.balloon4.x>900)
+             this.balloon4.x+=5;
+            if(this.balloon4.x>800)
                 {
                     this.balloon4.x=-150;
                 }
-            this.balloon5.x+=0.3;
+            this.balloon5.x+=3;
             if(this.balloon5.x>900)
                 {
                     this.balloon5.x=-150;
                 }
+            if(s==8)
+        {
+            console.log("game over");
+            this.text.setText("Your score: "+s+" You won!");
+        }
+   
         };
         window.onload = function() {     
     
         let config={
             type:Phaser.AUTO,
-            width:1000,
-            height:1000,
+            width:700,
+            height:950,
             scene: gameScene,
             physics:{
                 default: 'arcade',
